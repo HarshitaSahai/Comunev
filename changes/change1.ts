b92eb57181f0c7ad4413558bad6773a6a875c548
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2020-04-22 03:39:32 +0530

    Create change1.ts

---
+++ changes/change1.ts
@@ -0,0 +1,20 @@
+#src/app/app-routing.module.ts 
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
